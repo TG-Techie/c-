72f0b048a73568de53da31b80909945d5c094b89
rfc6058 <Hannavas360!>
2020-05-12 19:50:17 -0400

    unsure

--- inc_cmns_transpiler.py
+++ inc_cmns_transpiler.py
@@ -11,7 +11,7 @@ def namefromtree(nametree):
         tok = nametree.children[0]
         return str(tok), int(tok.line)
 
-def trans_literal(scope, litrl):
+def trans_literal(litrl):
     litrl = litrl.children[0]
     if litrl.data == 'number':
         num = litrl.children[0]
@@ -26,29 +26,19 @@ def trans_literal(scope, litrl):
         litrl_text = str(string).strip('"' + "'")
         return Litrl(strtype, f'''strlitrl("{litrl_text}")''')
     else:
-        raise CMNSCompileTimeError(f"invalid literal '{litrl.pretty()}'")
-
-def trans_func_call(scope, func, args):
-    if len(args) != len(func.args):
-        raise CMNSCompileTimeError(f"function '{func.name}' expects '{len(func.args)}' arguments got {len(args)}")
-    for index, arg in enumerate(args):
-        if arg.type != func.args[index].type:
-            raise CMNSCompileTimeError(f"incorrect type for argument {index} of expr '{func.name}'. expects a '{func.args[index].type.name} got {arg.type.name}'")
-    outargs = ','.join([f'{argexpr.outstr}' for argexpr in args])
-    return Expr(scope, func.type, f"{func.outname}({outargs})")
+        raise CMNSCompileTimeError(f"{litrl.pretty()} invalid literal")
 
 def trans_method_call(scope, expr, funcname, args):
     if funcname in expr.type.methods:
-        return trans_func_call(scope, expr.type.methods[funcname], (expr,) + args)
-        #outargs = ''.join([f', {argexpr.outstr}' for argexpr in args])
-        #return Expr(scope, expr.type.methods[funcname].type, f"{expr.type.methods[funcname].outname}({expr.outstr}{outargs})")
+        outargs = ''.join([f', {argexpr.outstr}' for argexpr in args])
+        return Expr(scope, expr.type.methods[funcname].type, f"{expr.type.methods[funcname].outname}({expr.outstr}{outargs})")
     else:
         raise CMNSCompileTimeError(f"line #lineno not implemented#: expr '{expr.outstr}' of type '{expr.type.name}' has no method '{funcname}'")
 
 def trans_expr(scope, expr):
     expr = expr.children[0] # all 'expr's only contain one child
     if expr.data == 'literal':
-        litrl = trans_literal(scope, expr)
+        litrl = trans_literal(expr)
         return Expr(scope, litrl.type, litrl.outstr)
     if expr.data == 'binop_expr':
         a = trans_expr(scope, expr.children[0])
@@ -77,7 +67,7 @@ def trans_stmt(scope, stmt):
         var = Var(scope, varname, expr.type)
         stmtmdl.lines.append(comment(f"line {lineno}: assign '{varname}'"))
         if var.name in scope:
-            if var.type != scope[var.name].type:
+            if var.type is not scope[var.name].type:
                 # casting can only happen in the local scope, each block is it's own scope
                 if static_typing:
                     raise CMNSCompileTimeError(f"line {lineno}: using static typing you cannot assign '{var.name}' declared as a '{scope[var.name].type.name}' to a '{var.type.name}'")
@@ -94,43 +84,28 @@ def trans_stmt(scope, stmt):
             scope.locals[var.name] = var
             stmtmdl.lines.append(comment(f"first assignment of '{var.name}' in scope"))
             stmtmdl.lines.append(f"{var.type.outname} {var.outname} = refto({expr.outstr});")
-    #elif stmt.data == 'if_stmt':
-    #
+    elif stmt.data == 'if_stmt':
+        print(stmt.pretty())
     else:
         print(stmt)
         raise NotImplementedError("unsupported stmt found")
     return stmtmdl
 
-def trans_func(scope, funcdef):
-    print([foo.data for foo in funcdef.children])
-
-def trans_stmt_block(scope, funcdef):
-    pass
-
-# for later / classdef
-def trans_funcdef_block(scope, funcdef):
-    pass
-
 def trans_module(foo):
     scope = Scope()
-    contents = []
-    lines = []
+    lines = []x
     for foo in foo.children:
         if foo.data == 'stmt':
-            contents.append(trans_stmt(scope, foo.children[0]).lines)
-        elif foo.data == 'funcdef':
-            contents.append(trans_func(scope, foo))
+            lines += trans_stmt(scope, foo.children[0]).lines
         else:
-            print(f"'{foo.data}'", foo.pretty())
             raise NotImplementedError(f"unsupported sentence '{foo.data}'")
+    return lines
 
 if __name__ == '__main__':
     paths =    ('./sentences/assign_int_lit.c-',
                 './sentences/binop_add.c-',
-                './sentences/funcdef.c-'
                 )
     for path in paths:
-        print(path)
         with open(path) as file:
             tree = parse(file.read())
             print(tree.pretty())
