726bc605e681b76da376f9800523fb2e707333e7
TG-Techie <TGTechie01@gmail.com>
2020-06-26 19:20:46 -0400

    item modeler working

--- cmns_model.py
+++ cmns_model.py
@@ -103,7 +103,7 @@ def model_func(path, tree, outer):
         ret_type = children.pop(0)
     else:
         ret_type = None
-    
+
     if kind == 'funcdef':
         model_to_make = Func
     elif kind == 'funcdec':
@@ -348,66 +348,3 @@ class Declaration(Item):
 
 class FuncDec(Func, Declaration):
     pass
-
-def test():
-    """
-    desc: a test of the transpiler on a bunch of sentences;
-    returns NoneType;
-    """
-    paths = (
-        "./sentences/assign_int_lit.c-",
-        "./sentences/binop_add.c-",
-        "./sentences/funcdef.c-",
-        "./sentences/ifstmt.c-",
-        "./sentences/nestedif.c-",
-        "./sentences/whileloop.c-",
-        "./sentences/methodcall.c-",
-        "./sentences/printtest.c-",
-        "./sentences/inputtest.c-",
-        "./sentences/linecont_docs.c-",
-        #"./sentences/classdef.c-",
-        #"./sentences/traitdef.c-",
-        #"./sentences/itemized.c-",
-        "./sentences/comptypes.c-",
-        #'./sentences/imports.c-',
-        "./sentences/inherit.c-",
-        "./sentences/traitcast.c-",
-    )
-    #paths = ("./sentences/plainclass.c-",)
-    error_paths = ("./sentences/casterror.c-",)
-    for path in paths:
-        print(f"\ntesting: '{path}'")
-        with open(path) as file:
-            text = file.read()
-            #print(text)
-
-            try:
-                tree = parse(text)
-                #print(tree.pretty())
-            except:
-                print(f"!parse error while parsing file: '{path}'")
-                raise
-
-            folder = path.replace('.c-', '')
-
-            if not os.path.exists(folder):
-                    os.makedirs(folder)
-
-            with open(folder+'/token_tree.txt', "w+") as file:
-                file.truncate(0)
-                file.write(tree.pretty())
-
-            try:
-                name = path.split('/')[-1][0:-3]
-                module = model_module(path, tree, name)
-            except:
-                print(f"error modeling module {path}")
-                raise
-
-            with open(folder+'/item_model.txt', "w+") as file:
-                file.truncate(0)
-                file.write(module._layout())
-                print(module._layout())
-
-if __name__ == "__main__":
-    test()
